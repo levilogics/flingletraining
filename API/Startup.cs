a57592bd266ebb1675679489f97f06923cb7f9f2
levilogics <levlar.logeco@gmail.com>
2023-04-13 06:56:19 +0800

    reformat code

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,34 +18,34 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
+
         public Startup(IConfiguration config)
         {
             _config = config;
         }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddIdentityServices(_config);
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("https://flingletraining.test");
+                });
+            });
 
-           AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-           services.AddCors(options =>
-           {
-               options.AddPolicy("CorsPolicy", builder =>
-               {
-                   builder.AllowAnyHeader()
-                       .AllowAnyMethod()
-                       .WithOrigins("https://flingletraining.test");
-               });
-           });
-
-           services.AddApplicationServices(_config);
-           services.AddControllers();
-           services.AddSwaggerGen(c =>
-           {
-               c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-           });
+            services.AddApplicationServices(_config);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
         }
-        
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors("CorsPolicy");
@@ -55,14 +55,12 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
+
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
