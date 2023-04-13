71262edfe9451826f6cf904f59ced3198c37bf56
levilogics <levlar.logeco@gmail.com>
2023-04-13 19:51:47 +0800

    fully defining the relationship

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,16 @@
-namespace API.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Photo
+namespace API.Entities
 {
-    public int Id { get; set; }
-    public string Url { get; set; }
-    public bool IsMain { get; set; }
-    public string? PublicId { get; set; }
+    [Table("Photos")]
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string? PublicId { get; set; }
+        
+        public AppUser AppUser { get; set; }
+        public int AppUserId { get; set; }
+    }   
 }
