9b6170d71a03727c73aaec5c957795a61ebf7b42
levilogics <levlar.logeco@gmail.com>
2023-04-13 08:16:39 +0800

    done on register home component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,19 +7,23 @@ import {FormsModule} from '@angular/forms';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import {NavComponent} from './nav/nav.component';
 import {HomeComponent} from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
