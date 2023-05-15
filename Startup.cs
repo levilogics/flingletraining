392bb1d948e653199e4e97b22a77de9fa7b01c76
levilogics <levlar.logeco@gmail.com>
2023-05-15 08:31:22 +0800

    changes
    Signed-off-by: levilogics <levlar.logeco@gmail.com>

--- Startup.cs
+++ Startup.cs
@@ -14,8 +14,6 @@ namespace API
             _config = config;
         }
 
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationServices(_config);
@@ -29,31 +27,19 @@ namespace API
             services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
                 .WithOrigins("https://localhost:4200"));
-
             app.UseAuthentication();
-
             app.UseAuthorization();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHub<PresenceHub>("hubs/presence");
-                endpoints.MapHub<MessageHub>("hubs/message");
-            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
